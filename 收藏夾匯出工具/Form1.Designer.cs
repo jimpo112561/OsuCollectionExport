ca20e01206fff78f721e13bb4c275d381683c583
jimpo112561 <f20001012@Gmail.com>
2018-10-29 00:09:36 +0800

    Add files via upload

---
+++ 收藏夾匯出工具/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace 收藏夾匯出工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lab_OsuPath = new System.Windows.Forms.Label();
+            this.btn_Export = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // lab_OsuPath
+            // 
+            this.lab_OsuPath.AutoSize = true;
+            this.lab_OsuPath.Location = new System.Drawing.Point(13, 13);
+            this.lab_OsuPath.Name = "lab_OsuPath";
+            this.lab_OsuPath.Size = new System.Drawing.Size(82, 12);
+            this.lab_OsuPath.TabIndex = 0;
+            this.lab_OsuPath.Text = "osu資料夾: N/A";
+            // 
+            // btn_Export
+            // 
+            this.btn_Export.Enabled = false;
+            this.btn_Export.Location = new System.Drawing.Point(357, 310);
+            this.btn_Export.Name = "btn_Export";
+            this.btn_Export.Size = new System.Drawing.Size(75, 23);
+            this.btn_Export.TabIndex = 2;
+            this.btn_Export.Text = "匯出";
+            this.btn_Export.UseVisualStyleBackColor = true;
+            this.btn_Export.Click += new System.EventHandler(this.btn_Export_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(15, 28);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(417, 276);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeCheck);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "json";
+            this.saveFileDialog1.Filter = "收藏夾|*.json";
+            this.saveFileDialog1.OverwritePrompt = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 338);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btn_Export);
+            this.Controls.Add(this.lab_OsuPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "收藏夾匯出工具  By 孤之界(jun112561)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lab_OsuPath;
+        private System.Windows.Forms.Button btn_Export;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ 收藏夾匯出工具/Form1.cs
@@ -0,0 +1,124 @@
+﻿using CollectionManager.DataTypes;
+using CollectionManager.Modules.FileIO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace 收藏夾匯出工具
+{
+    public partial class Form1 : Form
+    {
+        string osuPath; Collections collections;
+        public OsuFileIo osuFileIo = new OsuFileIo(new BeatmapExtension());
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            osuPath = OsuPathResolver.Instance.GetOsuDir((path) => {
+                var dialogResult = MessageBox.Show(
+                    "偵測到osu在: " + Environment.NewLine + path + Environment.NewLine + "是否正確?",
+                    "osu資料夾", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+                return dialogResult == DialogResult.Yes;
+            }, (text) => {
+                FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+                dialog.ShowNewFolderButton = false;
+                dialog.Description = "請選擇osu資料夾";
+                dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+                if (dialog.ShowDialog() == DialogResult.OK && Directory.Exists(dialog.SelectedPath)) return dialog.SelectedPath;
+                return "";
+            });
+
+            if (osuPath == string.Empty || !Directory.Exists(osuPath)) Exit("需要有效的osu資料夾才能繼續!");
+            if (!osuPath.EndsWith("\\")) osuPath += "\\";
+
+            lab_OsuPath.Text = "osu資料夾: " + osuPath;
+
+            osuFileIo.OsuSettings.Load(osuPath);
+            osuFileIo.OsuDatabase.Load(osuPath + "osu!.db");
+
+            collections = osuFileIo.CollectionLoader.LoadCollection(osuPath + "collection.db");
+
+            foreach (Collection item in collections)   
+            {
+                if (item.NumberOfBeatmaps != 0)
+                {
+                    TreeNode treeNode = treeView1.Nodes.Add(item.Name);
+                    foreach (BeatmapExtension item2 in item.AllBeatmaps())
+                    {
+                        treeNode.Nodes.Add(string.Format("{0} {1} - {2}", item2.MapSetId, item2.Title, item2.DiffName));
+                    }
+                }
+            }
+        }
+
+        private void treeView1_BeforeCheck(object sender, TreeViewCancelEventArgs e)
+        {
+            btn_Export.Enabled = false;
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            btn_Export.Enabled = true;
+        }
+
+        private void btn_Export_Click(object sender, EventArgs e)
+        {
+            TreeNode treeNode = treeView1.SelectedNode; JsonData jsonData;
+            if (treeNode == null) return;
+            if (treeNode.Parent != null) treeNode = treeNode.Parent;
+            saveFileDialog1.FileName = treeNode.Text;
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK) return;
+
+            if (File.Exists(saveFileDialog1.FileName) && MessageBox.Show("收藏夾資料已存在\r\n是否將新的收藏夾資料寫入到舊的收藏夾內?", "",MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                jsonData = JsonConvert.DeserializeObject<JsonData>(File.ReadAllText(saveFileDialog1.FileName));
+            }
+            else
+            {
+                jsonData = new JsonData();
+                jsonData.collection_data = new List<CollectionData>();
+            }
+
+            CollectionData collectionData = new CollectionData();
+            collectionData.collection_name = treeNode.Text;
+            collectionData.beatmap_data = new List<BeatmapData>();
+
+            foreach (BeatmapExtension item in collections.First((x) => x.Name == treeNode.Text).AllBeatmaps()) collectionData.beatmap_data.Add(new BeatmapData() { beatmap_id = item.MapId, beatmap_md5 = item.Md5, beatmap_name = item.Artist + " - " + item.Title, beatmap_setid = item.MapSetId });
+            jsonData.collection_data.Add(collectionData);
+
+            File.WriteAllText(saveFileDialog1.FileName, JsonConvert.SerializeObject(jsonData));
+
+            MessageBox.Show("完成!");
+        }
+
+        void Exit(string text)
+        {
+            MessageBox.Show(text);
+            Environment.Exit(0);
+        }
+    }
+
+    public class BeatmapData
+    {
+        public int beatmap_setid { get; set; }
+        public int beatmap_id { get; set; }
+        public string beatmap_name { get; set; }
+        public string beatmap_md5 { get; set; }
+    }
+
+    public class CollectionData
+    {
+        public string collection_name { get; set; }
+        public List<BeatmapData> beatmap_data { get; set; }
+    }
+
+    public class JsonData
+    {
+        public List<CollectionData> collection_data { get; set; }
+    }
+}
